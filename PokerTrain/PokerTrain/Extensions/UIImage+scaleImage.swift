c51e96fdbfbafafa1922c0252fda6f1dfaf658a9
Anton Mishkevich <mian19@protonmail.com>
2023-03-28 23:51:51 +0300

    finish learn screen

---
+++ PokerTrain/PokerTrain/Extensions/UIImage+scaleImage.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+scaleImage.swift
+//  Team Click
+//
+//  Created by Kyzu on 29.07.22.
+//
+
+import Foundation
+import UIKit
+
+extension UIImage {
+    
+    func resizeImageTo(size: CGSize) -> UIImage? {
+        
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        self.draw(in: CGRect(origin: CGPoint.zero, size: size))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return resizedImage
+    }
+}
